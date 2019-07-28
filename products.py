68f4aa8f23e8aac8bc44f6fb443708472cf4fdf9
Hung-Ming Wu <whungming@yahoo.com>
2019-07-28 12:54:35 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,16 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+    	    continue
+        name, price = line.strip().split(',')
+        products.append([name, price])
+
+print(products)
+
+
+#讓使用者輸入
 while True:
     name = input('please enter products: ')
     if name == 'q':
@@ -11,15 +23,18 @@ while True:
                       #p.append(price)
                       #products.append(p)
     products.append([name, price])
+print(products)
 #print(products)
 #print(products[0][0])
 #print(products[1][1])
 
+#印出所有購買記錄
 for p in products:
 	#print(p)     #show every list of name and price
 	#print(p[0])  #show every list of name
 	print(p[0], '的價格是', p[1])
 
+#寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f: #with is good for "close" in programming. 
                                      #'w' means write 
                                       #'r' means read
